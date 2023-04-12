daec98f2292a8122457f676ddb54b6834537bcba
ownedlotus <JonahRingdahl@outlook.com>
2023-04-12 19:01:45 -0400

    init, get, and print board scaffolding

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -23,7 +23,10 @@ class SudokuGenerator:
 	None
     '''
     def __init__(self, row_length, removed_cells):
-        pass
+        self.row_length = row_length
+        self.removed_cells = removed_cells
+        self.board = [[]]
+        self.board_length = math.sqrt(row_length)
 
     '''
 	Returns a 2D python list of numbers which represents the board
@@ -32,7 +35,7 @@ class SudokuGenerator:
 	Return: list[list]
     '''
     def get_board(self):
-        pass
+        return self.board
 
     '''
 	Displays the board to the console
@@ -42,7 +45,10 @@ class SudokuGenerator:
 	Return: None
     '''
     def print_board(self):
-        pass
+        for row in board:
+            for num in row:
+                print(num, end=' ')
+            print()
 
     '''
 	Determines if num is contained in the specified row (horizontal) of the board
