e6c1efd0c32988bd968cf7f04fc2aadbb402548f
ownedlotus <JonahRingdahl@outlook.com>
2023-04-22 13:32:04 -0400

    added const

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import sys
 import pygame
 
-#1080p screen size
-SCREEN_WIDTH = 1920
+#1080p screen size -> slightly smaller for even divisibility
+SCREEN_WIDTH = 1800
 SCREEN_HEIGHT = 1080
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Sudoku")
+
+def draw_grid():
+     for i in range(1, 9):
+          pass
+
+screen.fill(BLUE)
+draw_grid()
 
 while True:
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
                pygame.quit()
                sys.exit()
+               
+               
