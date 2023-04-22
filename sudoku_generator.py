1a867d02dff1ff00376db0cd78f2b1708c8083af
ownedlotus <JonahRingdahl@outlook.com>
2023-04-22 12:57:34 -0400

    Cell init

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -271,7 +271,11 @@ class Board:
 class Cell:
     
     def __init__(self, value, row, col, screen) -> None:
-        pass
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+        
     def set_cell_value(self, value):
         pass
     def set_sketched_value(self, value):
