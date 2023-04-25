c9c8671f4d5b85fa920b0d8d9112e9fe9ab731a7
giovanni-bejar <127358697+giovanni-bejar@users.noreply.github.com>
2023-04-25 11:18:22 -0400

    Update sudoku.py
    Added difficulty option which will make the main_menu() function return either 0,1,2 (easy, medium, hard).

--- sudoku.py
+++ sudoku.py
@@ -22,18 +22,78 @@ def start_game() -> pygame.Surface:
 def game_over():
      sys.exit(0)
      
+#Main menu, allows user to choose difficulty and returns it as 'difficulty'
 def main_menu(screen) -> int:
-     pygame.display.flip()
-     font = pygame.font.SysFont("monospace",15)
-     screen.fill(WHITE)
-     
-     label = font.render("Welcome to Sudoku", 1, WHITE)
-     label2 = font.render("Select a Game Mode:", 1, WHITE)
-     screen.blit(label, (100,100))
-     
-     #TODO replace for proper menu implementation
-     # difficulty = input('Please enter difficulty as 0,1,2 ')
-     # return difficulty
+    
+    # Define colors
+    WHITE = (255, 255, 255)
+    ORANGE = (255, 165, 0)
+
+    # Set up the window
+    WINDOW_SIZE = (900, 900)
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    pygame.display.set_caption("Sudoku")
+    screen.fill(WHITE)
+
+    # Draw the "Welcome to Sudoku" text
+    font = pygame.font.Font(None, 64)
+    text = font.render("Welcome to Sudoku", True, (0, 0, 0))
+    text_rect = text.get_rect(center=(WINDOW_SIZE[0]/2, 50))
+    screen.blit(text, text_rect)
+
+    # Draw the "Select Game Mode" text
+    font = pygame.font.Font(None, 48)
+    text_mode = font.render("Select Game Mode:", True, (0, 0, 0))
+    text_mode_rect = text_mode.get_rect(center=(WINDOW_SIZE[0]/2, 650))
+    screen.blit(text_mode, text_mode_rect)
+
+    # Draw the buttons
+    font = pygame.font.Font(None, 48)
+
+    button_width = 200
+    button_height = 75
+    screen_width = screen.get_width()
+    button_padding = (screen_width - (3 * button_width)) / 4
+
+    button_easy = pygame.Rect(button_padding, 750, button_width, button_height)
+    pygame.draw.rect(screen, ORANGE, button_easy)
+    text_easy = font.render("Easy", True, WHITE)
+    text_rect_easy = text_easy.get_rect(center=button_easy.center)
+    screen.blit(text_easy, text_rect_easy)
+
+    button_medium = pygame.Rect(button_padding * 2 + button_width, 750, button_width, button_height)
+    pygame.draw.rect(screen, ORANGE, button_medium)
+    text_medium = font.render("Medium", True, WHITE)
+    text_rect_medium = text_medium.get_rect(center=button_medium.center)
+    screen.blit(text_medium, text_rect_medium)
+
+    button_hard = pygame.Rect(button_padding * 3 + 2 * button_width, 750, button_width, button_height)
+    pygame.draw.rect(screen, ORANGE, button_hard)
+    text_hard = font.render("Hard", True, WHITE)
+    text_rect_hard = text_hard.get_rect(center=button_hard.center)
+    screen.blit(text_hard, text_rect_hard)
+
+    # Show the window
+    pygame.display.flip()
+
+    # Wait for user to press a button
+    difficulty = None
+    while difficulty is None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if button_easy.collidepoint(mouse_pos):
+                    difficulty = 0
+                elif button_medium.collidepoint(mouse_pos):
+                    difficulty = 1
+                elif button_hard.collidepoint(mouse_pos):
+                    difficulty = 2
+
+    # Return the difficulty value
+    return difficulty
 
 def game_in_progress(screen):
      #main_menu will display main menu and have user choose difficulty
